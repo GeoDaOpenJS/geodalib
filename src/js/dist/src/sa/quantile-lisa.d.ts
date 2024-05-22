38e1b435e6b9da8f410af69bb86a3984748d53eb
Xun Li <lixun910@gmail.com>
2024-05-22 10:28:45 -0700

    [Update] Change permutation method to complete

--- src/js/dist/src/sa/quantile-lisa.d.ts
+++ src/js/dist/src/sa/quantile-lisa.d.ts
@@ -8,4 +8,15 @@ export type QuantileLisaProps = {
     significanceCutoff?: number;
     seed?: number;
 };
+/**
+ * Get local Quantile Lisa statistics
+ * @param k The number of classes/categories
+ * @param quantile The quantile value
+ * @param data The numeric values to be classified.
+ * @param neighbors The neighbors of each observation
+ * @param permutation The number of permutations
+ * @param significanceCutoff The significance cutoff
+ * @param seed The seed value
+ * @returns LISA result
+ */
 export declare function quantileLisa({ k, quantile, data, neighbors, permutation, significanceCutoff, seed }: QuantileLisaProps): Promise<LocalMoranResult>;
