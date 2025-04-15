349580bb93e466974cfa165012819abcaa1e83d0
Xun Li <lixun910@gmail.com>
2025-04-14 20:33:33 -0700

    chore: fix esm build 'path' error

--- js/packages/regression/esbuild.config.mjs
+++ js/packages/regression/esbuild.config.mjs
@@ -1,12 +1,13 @@
 import { dtsPlugin } from 'esbuild-plugin-d.ts';
 import { createBaseConfig, buildFormat } from '../../esbuild.config.mjs';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 
 const baseConfig = createBaseConfig({
   target: ['es2020'],
   mainFields: ['module', 'main'],
   external: ['fs', 'path'],
   entryPoints: ['src/index.ts'],
-  plugins: [dtsPlugin()],
+  plugins: [dtsPlugin(), polyfillNode()],
 });
 
 // Build all formats
