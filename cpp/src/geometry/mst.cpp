432c79fce0a7469941039d0ef2113e8007a6cf19
Xun Li <lixun910@gmail.com>
2025-06-09 20:24:19 -0700

    fix: MST incorrect distance matrix (half) calcuation

--- cpp/src/geometry/mst.cpp
+++ cpp/src/geometry/mst.cpp
@@ -4,17 +4,19 @@ using namespace geoda;
 
 MST::MST(const std::vector<double>& x, const std::vector<double>& y, std::vector<double>& weights) : x_(x), y_(y) {
   // create distance matrix from points (only upper triangle needed due to symmetry)
-  distance_matrix_ = std::vector<std::vector<double>>(x_.size(), std::vector<double>(x_.size()));
-  for (size_t i = 0; i < x_.size(); i++) {
-    for (size_t j = i + 1; j < x_.size(); j++) {
-      double dist = std::sqrt(std::pow(x_[i] - x_[j], 2) + std::pow(y_[i] - y_[j], 2));
-      distance_matrix_[i][j] = dist;
-      distance_matrix_[j][i] = dist;  // mirror to lower triangle
-    }
-    distance_matrix_[i][i] = 0.0;  // diagonal is zero
-  }
-
   size_t num_obs = x_.size();
+  distance_matrix_.resize(num_obs);
+  for (size_t i = 0; i < num_obs; i++) {
+    distance_matrix_[i].resize(num_obs - i - 1);
+    for (size_t j = i + 1, k = 0; j < num_obs; j++, k++) {
+      double x1 = x_[i];
+      double y1 = y_[i];
+      double x2 = x_[j];
+      double y2 = y_[j];
+      double dist = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+      distance_matrix_[i][k] = std::sqrt(dist);
+    }
+  }
 
   BGraph g(num_obs);
   for (int i = 0; i < num_obs; i++) {
@@ -50,22 +52,23 @@ MST::MST(const std::vector<double>& x, const std::vector<double>& y, std::vector
   this->lines_.resize(num_edges);
 
   for (int i = 0; i < mst_edges.size(); i++) {
-    int f_idx = mst_edges[i].first;
-    int t_idx = mst_edges[i].second;
-    if (f_idx == t_idx) {
+    const std::pair<int, int>& s_t = mst_edges[i];
+    int source = s_t.first;
+    int target = s_t.second;
+    if (source == target) {
       continue;
     }
 
-    double cost = distance_matrix_[f_idx][t_idx];
+    double cost = distance_matrix_[source][target];
 
-    std::vector<double> x;
-    std::vector<double> y;
-    x.push_back(x_[f_idx]);
-    y.push_back(y_[f_idx]);
-    x.push_back(x_[t_idx]);
-    y.push_back(y_[t_idx]);
+    std::vector<double> x_coords;
+    std::vector<double> y_coords;
+    x_coords.push_back(x_[source]);
+    y_coords.push_back(y_[source]);
+    x_coords.push_back(x_[target]);
+    y_coords.push_back(y_[target]);
 
-    this->lines_[i].add(x, y);
+    this->lines_[i].add(x_coords, y_coords);
     weights.push_back(cost);
   }
 }
